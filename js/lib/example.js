490e944cbf77cd7a5b2bbf012358ce0e35b85610
aditya-giri <aditya.giridharan@gmail.com>
2020-12-02 22:28:47 +0530

    test pyQuirk widget

--- js/lib/example.js
+++ js/lib/example.js
@@ -19,10 +19,10 @@ var _ = require('lodash');
 
 // When serialiazing the entire widget state for embedding, only values that
 // differ from the defaults will be specified.
-var HelloModel = widgets.DOMWidgetModel.extend({
+var QuirkModel = widgets.DOMWidgetModel.extend({
     defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
-        _model_name : 'HelloModel',
-        _view_name : 'HelloView',
+        _model_name : 'QuirkModel',
+        _view_name : 'QuirkView',
         _model_module : 'pyQuirk',
         _view_module : 'pyQuirk',
         _model_module_version : '0.1.0',
@@ -33,23 +33,50 @@ var HelloModel = widgets.DOMWidgetModel.extend({
 
 
 // Custom View. Renders the widget model.
-var HelloView = widgets.DOMWidgetView.extend({
+var QuirkView = widgets.DOMWidgetView.extend({
     // Defines how the widget gets rendered into the DOM
     render: function() {
-        this.value_changed();
 
+        // this.email_input = document.createElement('input');
+        // this.email_input.type = 'email';
+        // this.email_input.value = this.model.get('value2');
+        // this.email_input.disabled = false;
+               
+        // this.el.appendChild(this.email_input);
+        //this.h1 = document.createElement('h1')
+        //this.h1.innerHTML = this.model.get('value2');
+        //this.el.appendChild(this.h1);
+        //console.log(this.model.get('value'))
+        this.frame = document.createElement('iframe');
+        this.frame.height = 500
+        this.frame.width = 1000
+        this.frame.src = 'pyQuirk/quirk.html#circuit={"cols":[["Z"],[1,"H","Y"]]}'
+        //frame.style.display = 'none';
+        //document.body.appendChild(frame);             
+        //frame.contentDocument.open();
+        //frame.srcdoc = this.model.get('value');
+        //frame.contentDocument.close();
+        //console.log(frame)
+        //var quirk = frame.contentDocument.body.firstChild;
+        //document.body.removeChild(frame);
+        console.log(this.frame)
+        this.el.appendChild(this.frame)
+        //this.el.innerHTML = document.createElement('<div><a href="#>text</a></div>'))
         // Observe changes in the value traitlet in Python, and define
         // a custom callback.
-        this.model.on('change:value', this.value_changed, this);
+        this.model.on('change:input_str', this.value_changed, this);
     },
 
     value_changed: function() {
-        this.el.textContent = this.model.get('value');
+        console.log(this.model.get('input_str'))
+        this.el.removeChild(this.frame)
+        this.frame.src = 'pyQuirk/quirk.html'+this.model.get('input_str');
+        this.el.appendChild(this.frame)
     }
 });
 
 
 module.exports = {
-    HelloModel: HelloModel,
-    HelloView: HelloView
+    QuirkModel,
+    QuirkView
 };
