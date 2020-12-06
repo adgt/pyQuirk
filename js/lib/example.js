9b507986c0c244b1162cbb783dee6e3f2e2ddf6b
aditya-giri <aditya.giridharan@gmail.com>
2020-12-06 19:26:57 +0530

    Update Quirk using QASM

--- js/lib/example.js
+++ js/lib/example.js
@@ -20,63 +20,41 @@ var _ = require('lodash');
 // When serialiazing the entire widget state for embedding, only values that
 // differ from the defaults will be specified.
 var QuirkModel = widgets.DOMWidgetModel.extend({
-    defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
-        _model_name : 'QuirkModel',
-        _view_name : 'QuirkView',
-        _model_module : 'pyQuirk',
-        _view_module : 'pyQuirk',
-        _model_module_version : '0.1.0',
-        _view_module_version : '0.1.0',
-        value : 'Hello World!'
-    })
+	defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
+		_model_name : 'QuirkModel',
+		_view_name : 'QuirkView',
+		_model_module : 'pyQuirk',
+		_view_module : 'pyQuirk',
+		_model_module_version : '0.1.0',
+		_view_module_version : '0.1.0',
+		value : 'Hello World!'
+	})
 });
 
 
-// Custom View. Renders the widget model.
 var QuirkView = widgets.DOMWidgetView.extend({
-    // Defines how the widget gets rendered into the DOM
-    render: function() {
 
-        // this.email_input = document.createElement('input');
-        // this.email_input.type = 'email';
-        // this.email_input.value = this.model.get('value2');
-        // this.email_input.disabled = false;
-               
-        // this.el.appendChild(this.email_input);
-        //this.h1 = document.createElement('h1')
-        //this.h1.innerHTML = this.model.get('value2');
-        //this.el.appendChild(this.h1);
-        //console.log(this.model.get('value'))
-        this.frame = document.createElement('iframe');
-        this.frame.height = 500
-        this.frame.width = 1000
-        this.frame.src = 'pyQuirk/quirk.html#circuit={"cols":[["Z"],[1,"H","Y"]]}'
-        //frame.style.display = 'none';
-        //document.body.appendChild(frame);             
-        //frame.contentDocument.open();
-        //frame.srcdoc = this.model.get('value');
-        //frame.contentDocument.close();
-        //console.log(frame)
-        //var quirk = frame.contentDocument.body.firstChild;
-        //document.body.removeChild(frame);
-        console.log(this.frame)
-        this.el.appendChild(this.frame)
-        //this.el.innerHTML = document.createElement('<div><a href="#>text</a></div>'))
-        // Observe changes in the value traitlet in Python, and define
-        // a custom callback.
-        this.model.on('change:input_str', this.value_changed, this);
-    },
+	render: function() {
 
-    value_changed: function() {
-        console.log(this.model.get('input_str'))
-        this.el.removeChild(this.frame)
-        this.frame.src = 'pyQuirk/quirk.html'+this.model.get('input_str');
-        this.el.appendChild(this.frame)
-    }
+		this.frame = document.createElement('iframe');
+		this.frame.id = "iframe";
+		this.frame.height = 350;
+		this.frame.width = 1000;
+		this.frame.src = 'pyQuirk/quirk.html';//#circuit={"cols":[["Z"],[1,"H","Y"]]}';
+		
+		this.el.appendChild(this.frame);
+
+		this.model.on('change:circuit_qasm', this.circuit_updated, this);
+	},
+
+	circuit_updated: function() {
+
+		this.frame.src = 'pyQuirk/quirk.html' + `#circuit={"cols":${this.model.get("circuit_qasm").replaceAll("'", "\"")}}`; // #circuit={"cols":[["Z"],[1,"H","Y"]]}';
+	}
 });
 
 
 module.exports = {
-    QuirkModel,
-    QuirkView
+	QuirkModel,
+	QuirkView
 };
--- pyQuirk/example.py
+++ pyQuirk/example.py
@@ -1,38 +1,38 @@
 import ipywidgets as widgets
 from traitlets import Unicode
-from .quirk import get_source_code
-
+from .quirk import qasm_to_quirk
 # See js/lib/example.js for the frontend counterpart to this file.
 
 @widgets.register
 class Quirk(widgets.DOMWidget):
-    """An example widget."""
+	"""An example widget."""
 
-    # Name of the widget view class in front-end
-    _view_name = Unicode('QuirkView').tag(sync=True)
+	# Name of the widget view class in front-end
+	_view_name = Unicode('QuirkView').tag(sync=True)
 
-    # Name of the widget model class in front-end
-    _model_name = Unicode('QuirkModel').tag(sync=True)
+	# Name of the widget model class in front-end
+	_model_name = Unicode('QuirkModel').tag(sync=True)
 
-    # Name of the front-end module containing widget view
-    _view_module = Unicode('pyQuirk').tag(sync=True)
+	# Name of the front-end module containing widget view
+	_view_module = Unicode('pyQuirk').tag(sync=True)
 
-    # Name of the front-end module containing widget model
-    _model_module = Unicode('pyQuirk').tag(sync=True)
+	# Name of the front-end module containing widget model
+	_model_module = Unicode('pyQuirk').tag(sync=True)
 
-    # Version of the front-end module containing widget view
-    _view_module_version = Unicode('^0.1.0').tag(sync=True)
-    # Version of the front-end module containing widget model
-    _model_module_version = Unicode('^0.1.0').tag(sync=True)
+	# Version of the front-end module containing widget view
+	_view_module_version = Unicode('^0.1.0').tag(sync=True)
+	# Version of the front-end module containing widget model
+	_model_module_version = Unicode('^0.1.0').tag(sync=True)
 
-    # Widget specific property.
-    # Widget properties are defined as traitlets. Any property tagged with `sync=True`
-    # is automatically synced to the frontend *any* time it changes in Python.
-    # It is synced back to Python from the frontend *any* time the model is touched.
-    value = Unicode(get_source_code()).tag(sync=True)
+	# Widget specific property.
+	# Widget properties are defined as traitlets. Any property tagged with `sync=True`
+	# is automatically synced to the frontend *any* time it changes in Python.
+	# It is synced back to Python from the frontend *any* time the model is touched.
+	# value = Unicode(get_source_code()).tag(sync=True)
 
-    input_str = Unicode("").tag(sync=True);
+	circuit_qasm = Unicode("").tag(sync=True)
 
-    def update_circuit(self, circuit_str):
-        self.input_str = circuit_str
+	def update_circuit(self, circuit):
+		self.circuit_qasm = qasm_to_quirk(circuit);
 
+	
